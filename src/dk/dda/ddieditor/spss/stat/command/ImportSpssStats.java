db51d53632292cb49f19d0531d26da6455123280
Jannik Vestergaard Jensen <jvj@dda.dk>
2012-01-26 16:20:06 +0000

    Initial commit
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-spss-stat@3124 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/dk/dda/ddieditor/spss/stat/command/ImportSpssStats.java
@@ -0,0 +1,18 @@
+package dk.dda.ddieditor.spss.stat.command;
+
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ImportSpssStats extends org.eclipse.core.commands.AbstractHandler {
+	private Log log = LogFactory.getLog(LogType.SYSTEM, ImportSpssStats.class);
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
