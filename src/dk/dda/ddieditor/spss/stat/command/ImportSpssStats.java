8fcdf277b5412123beb352b97de2b301be276807
Jannik Vestergaard Jensen <jvj@dda.dk>
2012-01-27 12:25:49 +0000

    Update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-spss-stat@3126 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/dk/dda/ddieditor/spss/stat/command/ImportSpssStats.java
+++ src/dk/dda/ddieditor/spss/stat/command/ImportSpssStats.java
@@ -5,14 +5,37 @@ import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.ui.PlatformUI;
+
+import dk.dda.ddieditor.spss.stat.wizard.SpssStatsWizard;
 
 public class ImportSpssStats extends org.eclipse.core.commands.AbstractHandler {
 	private Log log = LogFactory.getLog(LogType.SYSTEM, ImportSpssStats.class);
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		// open dialog
+		SpssStatsWizard statsWizard = new SpssStatsWizard();
+		WizardDialog dialog = new WizardDialog(PlatformUI.getWorkbench()
+				.getDisplay().getActiveShell(), statsWizard);
+
+		int returnCode = dialog.open();
+		if (returnCode != Window.CANCEL) {
+			// import
+			SpssStatsImportRunnable longJob = new SpssStatsImportRunnable(
+					statsWizard.selectedResource, statsWizard.outOxmlFile);
+			BusyIndicator.showWhile(PlatformUI.getWorkbench().getDisplay(),
+					longJob);
+
+			// refresh
+			// ViewManager.getInstance().addViewsToRefresh(
+			// new String[] { CodeSchemeEditor.ID,
+			// CategorySchemeEditor.ID, VariableSchemeEditor.ID });
+			// ViewManager.getInstance().refesh();
+		}
 		return null;
 	}
-
 }
