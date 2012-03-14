dbe8e37971a110851e4313b37e4e1cc0b4c5d4ee
Dan Kristiansen <dak@dda.dk>
2012-03-14 09:09:41 +0000

    Added ProgressService
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-spss-stat@3263 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/dk/dda/ddieditor/spss/stat/command/ImportSpssStats.java
+++ src/dk/dda/ddieditor/spss/stat/command/ImportSpssStats.java
@@ -1,13 +1,17 @@
 package dk.dda.ddieditor.spss.stat.command;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.ddialliance.ddiftp.util.Translator;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.ui.PlatformUI;
 
 import dk.dda.ddieditor.spss.stat.wizard.SpssStatsWizard;
@@ -25,16 +29,26 @@ public class ImportSpssStats extends org.eclipse.core.commands.AbstractHandler {
 		int returnCode = dialog.open();
 		if (returnCode != Window.CANCEL) {
 			// import
-			SpssStatsImportRunnable longJob = new SpssStatsImportRunnable(
+			final SpssStatsImportRunnable longJob = new SpssStatsImportRunnable(
 					statsWizard.selectedResource, statsWizard.inOxmlFile);
-			BusyIndicator.showWhile(PlatformUI.getWorkbench().getDisplay(),
-					longJob);
-
-			// refresh
-			// ViewManager.getInstance().addViewsToRefresh(
-			// new String[] { CodeSchemeEditor.ID,
-			// CategorySchemeEditor.ID, VariableSchemeEditor.ID });
-			// ViewManager.getInstance().refesh();
+			try {
+				PlatformUI.getWorkbench().getProgressService()
+						.busyCursorWhile(new IRunnableWithProgress() {
+							@Override
+							public void run(IProgressMonitor monitor)
+									throws InvocationTargetException,
+									InterruptedException {
+								monitor.beginTask(Translator
+										.trans("spssstat.wizard.title"), 1);
+								PlatformUI.getWorkbench().getDisplay()
+										.asyncExec(longJob);
+								monitor.worked(1);
+							}
+						});
+			} catch (Exception e) {
+				throw new ExecutionException(
+						Translator.trans("spssstat.error"), e.getCause());
+			}
 		}
 		return null;
 	}
